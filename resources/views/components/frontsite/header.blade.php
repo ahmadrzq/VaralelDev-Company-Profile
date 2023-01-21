1239cb79fed1c4ce53123c33bc30d95079cc3b7a
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-22 06:34:55 +0700

    Editing header

--- resources/views/components/frontsite/header.blade.php
+++ resources/views/components/frontsite/header.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
 
       <div id="logo" class="pull-left">
-        <h1><a href="#intro" class="scrollto">BizPage</a></h1>
+        <h1><a href="#intro" class="scrollto">VaralelDev</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="#intro"><img src="img/logo.png" alt="" title="" /></a>-->
       </div>
@@ -17,14 +17,14 @@
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#team">Team</a></li>
-          <li class="menu-has-children"><a href="">Drop Down</a>
+          {{-- <li class="menu-has-children"><a href="">Drop Down</a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li><a href="#">Drop Down 3</a></li>
               <li><a href="#">Drop Down 4</a></li>
               <li><a href="#">Drop Down 5</a></li>
             </ul>
-          </li>
+          </li> --}}
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav><!-- #nav-menu-container -->
--- resources/views/sections/frontsite/intro/index.blade.php
+++ resources/views/sections/frontsite/intro/index.blade.php
@@ -10,7 +10,7 @@
             <div class="carousel-inner" role="listbox">
 
                 <div class="carousel-item active">
-                    <div class="carousel-background"><img src="img/intro-carousel/1.jpg" alt=""></div>
+                    <div class="carousel-background"><img src="{{asset('assets/frontsite/img/intro-carousel/1.jpg')}}" alt=""></div>
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <h2>We are professional</h2>
@@ -25,7 +25,7 @@
                 </div>
 
                 <div class="carousel-item">
-                    <div class="carousel-background"><img src="img/intro-carousel/2.jpg" alt=""></div>
+                    <div class="carousel-background"><img src="{{asset('assets/frontsite/img/intro-carousel/2.jpg')}}" alt=""></div>
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <h2>At vero eos et accusamus</h2>
@@ -40,7 +40,7 @@
                 </div>
 
                 <div class="carousel-item">
-                    <div class="carousel-background"><img src="img/intro-carousel/3.jpg" alt=""></div>
+                    <div class="carousel-background"><img src="{{asset('assets/frontsite/img/intro-carousel/3.jpg')}}" alt=""></div>
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <h2>Temporibus autem quibusdam</h2>
@@ -55,7 +55,7 @@
                 </div>
 
                 <div class="carousel-item">
-                    <div class="carousel-background"><img src="img/intro-carousel/4.jpg" alt=""></div>
+                    <div class="carousel-background"><img src="{{asset('assets/frontsite/img/intro-carousel/4.jpg')}}" alt=""></div>
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <h2>Nam libero tempore</h2>
@@ -70,7 +70,7 @@
                 </div>
 
                 <div class="carousel-item">
-                    <div class="carousel-background"><img src="img/intro-carousel/5.jpg" alt=""></div>
+                    <div class="carousel-background"><img src="{{asset('assets/frontsite/img/intro-carousel/5.jpg')}}" alt=""></div>
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <h2>Magnam aliquam quaerat</h2>
