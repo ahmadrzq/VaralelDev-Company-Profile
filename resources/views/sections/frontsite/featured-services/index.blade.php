9a20278a1bc13ed5774b9b7f47779a6eecbd727c
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-22 19:01:17 +0700

    Make box-bg class active on mid card

--- resources/views/sections/frontsite/featured-services/index.blade.php
+++ resources/views/sections/frontsite/featured-services/index.blade.php
@@ -6,13 +6,13 @@
         <div class="row">
 
             @forelse ($filter_services as $key => $filter_services_item)
-                <div class="col-lg-4 box box-bg">
+                <div class="col-lg-4 box {{$filter_services_item->id == 2 ? 'box-bg' : ''}}">
                     <i class="ion-ios-stopwatch-outline"></i>
                     <h4 class="title"><a href="">{{ $filter_services_item->title ?? '' }}</a></h4>
                     <p class="description">{{ $filter_services_item->description ?? '' }}</p>
                 </div>
             @empty
-            
+
             @endforelse
 
         </div>
