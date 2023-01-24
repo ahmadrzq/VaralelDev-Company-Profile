c2e0a7eb77491c28b9e91b2f0a2e34d3a2130442
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-24 22:00:35 +0700

    Update migration portfolios

--- database/migrations/2023_01_21_145424_create_portfolios_table.php
+++ database/migrations/2023_01_21_145424_create_portfolios_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
+            $table->string('category_id');
+            $table->string('name');
+            $table->string('photo');
             $table->timestamps();
         });
     }
