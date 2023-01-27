b0bfcd99893e75b7b2bb344487e4b1ea67c7a932
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-27 23:56:45 +0700

    Update migration for contact

--- database/migrations/2023_01_21_152654_create_contacts_table.php
+++ database/migrations/2023_01_21_152654_create_contacts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('email');
+            $table->string('phone');
+            $table->text('address');
             $table->timestamps();
         });
     }
