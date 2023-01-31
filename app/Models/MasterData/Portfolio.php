d21c4bf1628709436dd8bfe09d4bd6e5f4571df0
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-31 23:34:37 +0700

    Update model portfolio

--- app/Models/MasterData/Portfolio.php
+++ app/Models/MasterData/Portfolio.php
@@ -2,10 +2,34 @@
 
 namespace App\Models\MasterData;
 
+use Database\Factories\MasterData\PortfolioFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
+    //use factory
     use HasFactory;
+    protected static function newFactory()
+    {
+        return PortfolioFactory::new();
+    }
+
+    //declare table
+    // public $table = 'intro';
+
+    //this type data must be yyyy-mm-dd
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    //declare fillable
+    protected $fillable = [
+        'category_id',
+        'name',
+        'photo',
+        'created_at',
+        'updated_at'
+    ];
 }
